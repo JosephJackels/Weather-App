a6dd937ce4ae2472d1e49eceee0dda25dee21093
Joseph Jackels <jackelsjoseph@gmail.com>
2020-07-13 18:03:54 -0500

    Rework app to always request both current and forecast weather, and display both. will edit styling in future to finish rework

--- formHandling.js
+++ formHandling.js
@@ -2,42 +2,34 @@
 This file contains global variables
 	searchString - used for setting the full query string, including query type and query, when making a request from the openweathermap api
 	units - used to set which unit type the query should be returned in
-	weatherType - used to differentiate between a request for current weather and forecasted weather
 
 This file adds event listeners
 	unitType - sets the global variable unit when the selection is changed
-	weatherType - sets the global variable weatherType when the selection is changed
 
 This file contains functions
 	updateUnits - sets the global variable units
-	updateWeatherType - sets the global variable weatherType
 	setSearch - cleans page by removing the old error messages and weather requests, sets the global variable searchString - including the appropriate query type and query value - and makes a request if the searchString is set
 	makeRequest - begin a new request from the openweatherapi
-	searchFailed - shows an error message when the search input is not parsed as a zip code or a city name
+	createErrorMessage - given a string and an element, creates a p tag with the given string, setes the errorMessage class, and appends the error element to the given element
+	searchFailed - creates an error message when the search input is not parsed as a zip code or a city name
 	removeOldErrorMessages - checks the document for existing error messages and removes them
 	removeElement - given an element, remove that element from the document by removing it from the list of children of its parent node
 */
 
 var searchString;
 var units = document.getElementById('unitType').value;
-var weatherType = document.getElementById('weatherType').value;
 
 //document.getElementById('searchType').addEventListener('change', updateSearchForm);
 document.getElementById('unitType').addEventListener('change', updateUnits);
-document.getElementById('weatherType').addEventListener('change', updateWeatherType);
 
 function updateUnits(unitInput){
 	//sets the proper unit value when a different option is selected
 	units = unitInput.target.value;
 }
 
-function updateWeatherType(typeInput){
-	//updates weather search type - forecast or current
-	weatherType = typeInput.target.value;
-}
 function setSearch(form){
 	//builds search string to be sent to api based upon form values
-	removeoldErrorMessages();
+	removeOldErrorMessages();
 	removeOldRequest();
 
 	var inputString = form.searchInput.value;
@@ -59,14 +51,17 @@ function setSearch(form){
 function makeRequest(){
 	getWeather();
 }
-function searchFailed(){
+function createErrorMessage(errorString, appendTo){
 	var errorMessage = document.createElement('p');
-	errorMessage.setAttribute('id', 'errorParsing');
 	errorMessage.classList.add('errorMessage');
-	errorMessage.innerText = "Search did not parse as ZIP or City Name. ZIP should be in format #####, #####[.-+ ]####, or #########. City name should only contain letters, with multiple word names seperated by a single space between words. Must start and end with a letter.";
-	document.getElementById("searchForm").appendChild(errorMessage);
+	errorMessage.innerText = errorString; 
+	appendTo.appendChild(errorMessage);
 }
-function removeoldErrorMessages(){
+function searchFailed(){
+	var message = "Search did not parse as ZIP or City Name. ZIP should be in format #####, #####[.-+ ]####, or #########. City name should only contain letters, with multiple word names seperated by a single space between words. Must start and end with a letter.";
+	createErrorMessage(message, document.getElementById("searchForm"));
+}
+function removeOldErrorMessages(){
 	var errorList = document.querySelectorAll('.errorMessage');
 	if(errorList != null){
 		errorList.forEach(error => removeElement(error));
