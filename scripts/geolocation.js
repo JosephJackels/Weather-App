37409b5679be664eb7262d59b43960cd1b81dd6c
Joseph Jackels <jackelsjoseph@gmail.com>
2020-08-08 21:20:03 -0500

    added checkbox inputs for toggling map layers

--- scripts/geolocation.js
+++ scripts/geolocation.js
@@ -11,9 +11,11 @@ This file contains global variables:
 
 This file add event listeners:
 	map.on('moveend') - when scrolling/zooming/panning map, make request when move ends
+	layerInputs - event listener for checkboxes controling which weather layers are overlaid on map
 
 This file contains functions:
 	moveMap - function used to move a map to desired coordinates
+	setLayer - sets a given layer's visibility to the value of its checkbox
 */
 
 //init coords set to St Paul, MN. init coords are used when geolocation is unavailable/to set map until geolocation is approved,
@@ -70,9 +72,29 @@ map.on('moveend', function(){
 
 function moveMap(lon, lat){
 	var newCenter = ol.proj.fromLonLat([lon, lat]);
-	console.log(lon + ', ' + lat);
 	view.animate({
 		center: newCenter,
 		duration: 2000,
 	});
+}
+
+var layerInputs = document.querySelectorAll('.layerSelector');
+layerInputs.forEach(input => input.addEventListener('change', (event) => {
+	setLayer(event.target.name);
+}));
+
+function setLayer(layer){
+	switch(layer){
+		case 'tempLayer':
+			tempLayer.setVisible(document.getElementById('tempLayer').checked);
+		break;
+		case 'cloudLayer':
+			cloudLayer.setVisible(document.getElementById('cloudLayer').checked);
+		break;
+		case 'precipLayer':
+			precipLayer.setVisible(document.getElementById('precipLayer').checked);
+		break;
+		default:
+		break;
+	}
 }
